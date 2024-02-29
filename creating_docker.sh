a8a092151b7f53b1d0ec201aa80ad68322021ad2
cdrojasm <cdrojasm@unal.edu.co>
2024-02-28 23:30:50 -0500

    finishing configuration

--- creating_docker.sh
+++ creating_docker.sh
@@ -8,11 +8,15 @@ echo "$VOLUME_PATH_HOST"
 echo "$VOLUME_PATH_CONTAINER"
 echo "$IMAGE_CONTAINER"
 
-docker build --no-cache . -t $IMAGE_CONTAINER
-if [ $? -eq 0 ];then
-	echo "image was created successfully"
-else
-	echo "image was not created PANICCCC!!!"
-	exit 1
+docker images | grep "$IMAGE_CONTAINER" | wc -l 
+if [ $? -eq "0" ];then
+	echo "building the image"
+	docker build --no-cache . -t $IMAGE_CONTAINER
+	if [ $? -eq 0 ];then
+		echo "image was created successfully"
+	else
+		echo "image was not created PANICCCC!!!"
+		exit 1
+	fi
 fi
 docker run -dt --name $CONTAINER_NAME -v $VOLUME_PATH_HOST:$VOLUME_PATH_CONTAINER $IMAGE_CONTAINER
